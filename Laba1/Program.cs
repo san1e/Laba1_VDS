dee3fa331a6fdb22c7de4a09a309447c508f42bb
Олександр Клименко <alklimenko227@outlook.com>
2023-03-05 20:08:07 +0200

    Добавьте файлы проекта.

---
+++ Laba1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Laba1
+{
+    internal class Program
+    {
+        static string DualSys(int num)
+        {
+            int[] DualSys = new int[9];
+            for (int i = 0; i < DualSys.Length; i++)
+            {
+                int Rem = num % 2;
+                int num1 = num / 2;
+                num = num1;
+                DualSys[i] = Rem;
+            }
+
+            string str = string.Join(" ", DualSys.Reverse());
+            return str;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть число вiд 0 до 65535");
+            Console.Write("Десяткова система = ");
+            int num = int.Parse(Console.ReadLine());
+            string str = DualSys(num);
+            Console.WriteLine($"Двiйкова  система = {str}");
+        }
+    }
+}
